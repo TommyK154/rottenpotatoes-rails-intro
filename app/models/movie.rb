88b5a695b996d36edb2470adf8d93e66c1a4db1c
tommyk154 <tkargul1@binghamton.edu>
2016-02-22 03:13:25 +0000

    setting up rating radio buttons

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,6 @@
 class Movie < ActiveRecord::Base
-    def self.all_rating 
-        ['G', 'PG', 'PG-13', 'R']
-    end
+    @all_ratings = ['G', 'PG', 'PG-13', 'R']
+    #def self.all_ratings 
+     #   ['G', 'PG', 'PG-13', 'R']
+    #end
 end
